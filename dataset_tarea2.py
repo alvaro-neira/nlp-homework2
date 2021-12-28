7804db10c3e5610107072cc203b74045335584c9
Alvaro Neira <aneira@proactiveoffice.com>
2021-12-28 20:27:07 -0300

    train/validation

---
+++ dataset_tarea2.py
@@ -0,0 +1,56 @@
+import csv
+
+import datasets
+from datasets.tasks import TextClassification
+
+
+_DESCRIPTION = """\
+A Description
+"""
+
+_CITATION = """\
+"""
+
+_TRAIN_DOWNLOAD_URL = "https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/master/data/ag_news_csv/train.csv"
+_TEST_DOWNLOAD_URL = "https://raw.githubusercontent.com/mhjabreel/CharCnn_Keras/master/data/ag_news_csv/test.csv"
+
+
+class AGNews(datasets.GeneratorBasedBuilder):
+    """AG News topic classification dataset."""
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "text": datasets.Value("string"),
+                    "label": datasets.features.ClassLabel(names=["World", "Sports", "Business", "Sci/Tech"]),
+                }
+            ),
+            homepage="http://groups.di.unipi.it/~gulli/AG_corpus_of_news_articles.html",
+            citation=_CITATION,
+            task_templates=[TextClassification(text_column="text", label_column="label")],
+        )
+
+    def _split_generators(self, dl_manager):
+        train_path = dl_manager.download_and_extract(_TRAIN_DOWNLOAD_URL)
+        test_path = dl_manager.download_and_extract(_TEST_DOWNLOAD_URL)
+        return [
+            datasets.SplitGenerator(name=datasets.Split.TRAIN, gen_kwargs={"filepath": train_path}),
+            datasets.SplitGenerator(name=datasets.Split.TEST, gen_kwargs={"filepath": test_path}),
+        ]
+
+    def _generate_examples(self, filepath):
+        """Generate AG News examples."""
+        with open(filepath, encoding="utf-8") as csv_file:
+            csv_reader = csv.reader(
+                csv_file, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True
+            )
+            for id_, row in enumerate(csv_reader):
+                label, title, description = row
+                # Original labels are [1, 2, 3, 4] ->
+                #                   ['World', 'Sports', 'Business', 'Sci/Tech']
+                # Re-map to [0, 1, 2, 3].
+                label = int(label) - 1
+                text = " ".join((title, description))
+                yield id_, {"text": text, "label": label}
